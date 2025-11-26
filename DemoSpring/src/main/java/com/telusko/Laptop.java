d0239ac398851f80c467548b2b249b38ff80c3bb
Ankur Yadav <ankuryadav7559@gmail.com>
2025-11-26 20:58:41 +0530

    refactor : maintaining the structure of code

---
+++ DemoSpring/src/main/java/com/telusko/Laptop.java
@@ -0,0 +1,12 @@
+package com.telusko;
+
+public class Laptop {
+
+    public Laptop(){
+        System.out.println("Laptop constructor");
+    }
+
+    public void compile(){
+        System.out.println("Compiling");
+    }
+}
